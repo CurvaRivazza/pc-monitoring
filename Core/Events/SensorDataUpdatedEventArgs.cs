0f439e6004b09f73abd0a335c7fe1d0eb8f0b89c
S0s0Combain <kudrinskiy@internet.ru>
2025-10-11 00:34:01 +0300

    Добавить событие SensorDataUpdatedEventArgs

---
+++ Core/Events/SensorDataUpdatedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Events
+{
+    // событие, возникающее при каждом обновлении данных
+    public class SensorDataUpdatedEventArgs : EventArgs
+    {
+        public IEnumerable<SensorData> Data { get; }
+        
+        public SensorDataUpdatedEventArgs(IEnumerable<SensorData> data) 
+        {
+            Data = data;
+        }
+    }
+}
