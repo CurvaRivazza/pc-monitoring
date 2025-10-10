25d56fba35cc6e241292136e5245831a6c2ad050
S0s0Combain <kudrinskiy@internet.ru>
2025-10-11 00:02:48 +0300

    Опишите перечисление SensorTypes в Core

---
+++ Core/Enums/SensorTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Enums
+{
+    // типы показаний датчиков
+    public enum SensorTypes
+    {
+        Temperature,
+        Load,
+        Voltage,
+        Clock,
+        Power
+    }
+}
