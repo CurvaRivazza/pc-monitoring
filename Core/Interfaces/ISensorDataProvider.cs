261b942677519ba02707734a631c1ed115f5592a
S0s0Combain <kudrinskiy@internet.ru>
2025-10-11 00:08:15 +0300

    Опишите интерфейс ISensorDataProvider в Core

---
+++ Core/Interfaces/ISensorDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    // абстракция для источника данных с датчиков
+    public interface ISensorDataProvider
+    {
+        Task<IEnumerable<SensorData>> GetSensorDataAsync();
+    }
+}
