fbcf8ac80be93aa45d99d93646e445c237a5d11a
S0s0Combain <kudrinskiy@internet.ru>
2025-10-11 00:06:17 +0300

    Опишите класс SensorData в Core

---
+++ Core/Models/SensorData.cs
@@ -0,0 +1,19 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    // модель для показания датчика
+    public class SensorData
+    {
+        public string Name { get; set; }
+        public SensorTypes Type { get; set; }
+        float Value { get; set; }  // текущее значение
+        string Identifier { get; set; }  // идентификатор датчика в системе
+        string ParentName { get; set; }  // название компонента владельца
+    }
+}
