dce5ee83c6854705ffd2b1559807e978dc483a67
S0s0Combain <kudrinskiy@internet.ru>
2025-10-11 23:42:09 +0300

    Зарегистрируйте сервисы в App.xaml.cs

--- WpfApp/App.xaml.cs
+++ WpfApp/App.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sensors.Providers;
+using Sensors.Services;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfApp.ViewModels;
 
 namespace WpfApp
 {
@@ -13,5 +19,40 @@ namespace WpfApp
     /// </summary>
     public partial class App : Application
     {
+        private IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder().ConfigureServices(ConfigureServices).Build();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // регистрация сервисов
+            services.AddSingleton<ISensorDataProvider, LibreHardwareMonitorProvider>();
+            services.AddSingleton<SensorMonitoringService>();
+
+            // регистрация ViewModels
+            services.AddSingleton<CpuMonitorViewModel>();
+            services.AddSingleton<MainViewModel>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.DataContext = _host.Services.GetRequiredService<MainViewModel>();
+            mainWindow.Show();
+            
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host.StopAsync(TimeSpan.FromSeconds(5));
+            }
+            base.OnExit(e);
+        }
     }
 }
