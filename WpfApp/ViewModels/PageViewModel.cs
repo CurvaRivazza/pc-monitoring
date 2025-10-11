f4be450526295f5a4cd730032599beb7897b2126
S0s0Combain <kudrinskiy@internet.ru>
2025-10-11 22:55:51 +0300

    Реализуйте базовую ViewModel для страниц PageViewModel

---
+++ WpfApp/ViewModels/PageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.ViewModels
+{
+    // базовый класс с общей логикой для всех ViewModel страниц
+    public abstract class PageViewModel : ObservableObject
+    {
+        public abstract string Title { get; }  // заголовок страницы для отображения в навигации
+    }
+}
