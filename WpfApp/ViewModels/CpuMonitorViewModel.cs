2f64b7f0ca3ed76aea4cd25966d401bac088b56a
S0s0Combain <kudrinskiy@internet.ru>
2025-10-12 22:58:08 +0300

    Замените ObservableProperty на ручную реализацию публичных полей в CpuMonitorViewModel и реализуйте метод OnSensorDataUpdated

--- WpfApp/ViewModels/CpuMonitorViewModel.cs
+++ WpfApp/ViewModels/CpuMonitorViewModel.cs
@@ -6,6 +6,7 @@ using Sensors.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,17 +19,37 @@ namespace WpfApp.ViewModels
         public override string Title => "Процессор";
         private readonly SensorMonitoringService _sensorMonitoringService;
 
-        [ObservableProperty]
         private ObservableCollection<SensorData> _cpuSensors = new ObservableCollection<SensorData>();  // коллекция датчиков, относящихся к процессору
 
-        [ObservableProperty]
         private float _cpuTemperature;
 
-        [ObservableProperty]
         private float _cpuLoad;
 
-        [ObservableProperty]
         private float _cpuPower;
+
+        public ObservableCollection<SensorData> CpuSensors
+        {
+            get => _cpuSensors;
+            set => SetProperty(ref _cpuSensors, value);
+        }
+
+        public float CpuTemperature
+        {
+            get => _cpuTemperature;
+            set => SetProperty(ref _cpuTemperature, value);
+        }
+
+        public float CpuLoad
+        {
+            get => _cpuLoad;
+            set => SetProperty(ref _cpuLoad, value);
+        }
+
+        public float CpuPower
+        {
+            get => _cpuPower;
+            set => SetProperty(ref _cpuPower, value);
+        }
 
         public CpuMonitorViewModel(SensorMonitoringService sensorMonitoringService)
         {
@@ -39,22 +60,41 @@ namespace WpfApp.ViewModels
 
         private void OnSensorDataUpdated(object sender, SensorDataUpdatedEventArgs e)
         {
+            IEnumerable<SensorData> cpuSensors = e.Data.Where(s =>
+        s.Name?.ToLower().Contains("cpu") == true ||
+        s.ParentName?.ToLower().Contains("processor") == true ||
+        s.Name?.ToLower().Contains("package") == true ||         
+        s.Name?.ToLower().Contains("tdie") == true ||            
+        s.Name?.ToLower().Contains("tctl") == true);
 
+            System.Windows.Application.Current.Dispatcher.Invoke(() =>
+            {
+                CpuSensors.Clear();
+                var uniqueSensors = cpuSensors.GroupBy(s => s.Name)
+                                    .Select(g => g.First())
+                                    .ToList();
+                foreach (SensorData sensor in uniqueSensors)
+                {
+                    CpuSensors.Add(sensor);
+                }
+
+                UpdateMainIndicators(cpuSensors);
+            });
         }
 
         private void UpdateMainIndicators(IEnumerable<SensorData> data)
         {
             // поиск максимальной температуры процессора
             IEnumerable<SensorData> tempSensors = data.Where(s => s.Type == SensorTypes.Temperature);
-            _cpuTemperature = tempSensors.Any() ? tempSensors.Max(s => s.Value) : 0;
+            CpuTemperature = tempSensors.Any() ? tempSensors.Max(s => s.Value) : 0;
 
             // поиск общей загрузки процессора
             IEnumerable<SensorData> loadSensors = data.Where(s => s.Type == SensorTypes.Load && s.Name.ToLower().Contains("total"));
-            _cpuLoad = loadSensors.Any() ? loadSensors.Max(s => s.Value) : 0;
+            CpuLoad = loadSensors.Any() ? loadSensors.Max(s => s.Value) : 0;
 
             // поиск мощности процессора
             IEnumerable<SensorData> powerSensors = data.Where(s => s.Type == SensorTypes.Power);
-            _cpuPower = powerSensors.Any() ? powerSensors.Max(s => s.Value) : 0;
+            CpuPower = powerSensors.Any() ? powerSensors.Max(s => s.Value) : 0;
         }
     }
 }
