184499ba41ee168a9595827ed5c6d52c956eac7d
S0s0Combain <kudrinskiy@internet.ru>
2025-10-12 23:07:43 +0300

    Добавьте gif с демонстрацией функциональности приложения для оформления README

---
+++ WpfApp/Converters/LoadProgressBarVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApp.Converters
+{
+    // конвертер для отображения прогрессбара только для датчиков типа Load
+    public class LoadProgressBarVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is SensorTypes sensorType)
+            {
+                return sensorType == SensorTypes.Load ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
