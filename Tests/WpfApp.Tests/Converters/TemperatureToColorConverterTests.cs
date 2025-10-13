4047076fb2e54bff1601e7fe4fc14945703f45a0
S0s0Combain <kudrinskiy@internet.ru>
2025-10-14 00:24:34 +0300

    Добавьте тесты конвертера температуры для диапазона средних температур

--- Tests/WpfApp.Tests/Converters/TemperatureToColorConverterTests.cs
+++ Tests/WpfApp.Tests/Converters/TemperatureToColorConverterTests.cs
@@ -29,5 +29,16 @@ namespace Tests.WpfApp.Tests.Converters
 
             Assert.That(result, Is.EqualTo(Brushes.Green), $"Температура {temperature}°C должна возвращать зеленый цвет");
         }
+
+        [TestCase(60f)]
+        [TestCase(65.5f)]
+        [TestCase(70f)]
+        [TestCase(79.9f)]
+        public void Convert_TemperatureIsMedium_ReturnsOrange(float temperature)
+        {
+            Object? result = _converter.Convert(temperature, typeof(Brush), null, null);
+
+            Assert.That(result, Is.EqualTo(Brushes.Orange), $"Температура {temperature}°C должна возвращать оранжевый цвет");
+        }
     }
 }
