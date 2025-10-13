2dddf4efa8fe0ecf0f0c4abe6fdad3e6fa791155
S0s0Combain <kudrinskiy@internet.ru>
2025-10-14 00:19:35 +0300

    Добавьте тесты конвертера температуры для диапазона низких температур

---
+++ Tests/WpfApp.Tests/Converters/TemperatureToColorConverterTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using WpfApp.Converters;
+
+namespace Tests.WpfApp.Tests.Converters
+{
+    [TestFixture]
+    public class TemperatureToColorConverterTests
+    {
+        private TemperatureToColorConverter _converter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _converter = new TemperatureToColorConverter();
+        }
+
+        [TestCase(0f)]
+        [TestCase(30f)]
+        [TestCase(45.5f)]
+        [TestCase(59.9f)]
+        public void Convert_TemperatureIsLow_ReturnsGreen(float temperature)
+        {
+            Object? result = _converter.Convert(temperature, typeof(Brush), null, null);
+
+            Assert.That(result, Is.EqualTo(Brushes.Green), $"Температура {temperature}°C должна возвращать зеленый цвет");
+        }
+    }
+}
